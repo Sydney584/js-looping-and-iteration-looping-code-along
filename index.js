a927e5fa879aba55379d669134759bf0c23552ec
sydney <sydneychase5841@gmail.com>
2021-04-06 17:33:15 -0400

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+  
+}
+const names = ['Lisa', 'Kaitlin', 'Jan']
+const event = 'surprise'
+function writeCards(names, event) {
+    const messages = []
+
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+    return messages
+}
 // Code your solutions in this file
